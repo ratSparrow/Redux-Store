144d02c9e9817a791234ed40db4ea5635e6f7c21
Md.Rafiul <mrafiul.alam7@gmail.com>
2025-01-04 19:14:06 +0600

    updated redux part

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-const incrementEl = document.getElementById("increment")
-const decrementEl = document.getElementById("decrement")
-const counter = document.getElementById("counter")
+const counterEl = document.getElementById('counter')
+const incrementEl = document.getElementById('increment')
+const decrementEl = document.getElementById('decrement')
 
-let count = 0
+const initialState = {
+    value:0
+}
 
-incrementEl.addEventListener('click',()=>{
-    count++
-    counter.innerText = count
+const counterReducer = (state=initialState, action)=>{
+    if (action.type === 'increment'){
+        return {
+            ...state,
+            value:state.value+1
+        }
+    }
+    else if(action.type === 'decrement'){
+        return {
+            ...state,
+            value:state.value-1
+        }
+    }else{
+        return {
+            ...state
+        }
+    }
+}
+
+const store = Redux.createStore(counterReducer)
+
+const render = () =>{
+    const state = store.getState()
+    counterEl.innerText = state.value.toString()
+}
+
+render()
+
+store.subscribe(render)
+
+incrementEl.addEventListener('click', ()=>{
+    store.dispatch({
+        type:'increment'
+    })
 })
-decrementEl.addEventListener('click',()=>{
-    count--
-    counter.innerText = count
+
+decrementEl.addEventListener('click', ()=>{
+    store.dispatch({
+        type:'decrement'
+    })
 })
