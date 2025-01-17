3c53b7e64a76d2db75050f808e0b221aeadd2685
Md.Rafiul <mrafiul.alam7@gmail.com>
2025-01-17 21:52:46 +0600

    updated

--- redux-setup/src/redux/store.js
+++ redux-setup/src/redux/store.js
@@ -1,9 +1,18 @@
 
-import { createStore} from "redux"
+import { applyMiddleware, createStore} from "redux"
 
 import rootReducer from "./rootReducer"
 
-const store = createStore(rootReducer)
+const myLogger = (store) => (next) => (action) =>{
+    console.log(JSON.stringify(action))
+    console.log(JSON.stringify(store.getState()))
+
+    const upcomingState = [action].reduce(rootReducer, store.getState())
+    console.log(JSON.stringify(upcomingState))
+
+    return next()
+}
+const store = createStore(rootReducer,applyMiddleware(myLogger))
 
 
 export default store
