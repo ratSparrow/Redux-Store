ec8669231c67893e00aae3aa7373fc19f70bf98f
Md.Rafiul <mrafiul.alam7@gmail.com>
2025-01-23 15:05:46 +0600

    updated

--- redux-todo-list/src/components/main/TodoList.jsx
+++ redux-todo-list/src/components/main/TodoList.jsx
@@ -3,13 +3,38 @@ import Todo from "./Todo"
 
 const TodoList = () => {
   const todos = useSelector(state => state.todos)
-  // console.log(todos)
+  const filters = useSelector(state=> state.filters)
+  const {status, colors} = filters
+
+  const filterTodoByStatus = (todo)=>{
+    switch (status) {
+      case 'Complete':
+        return todo.completed
+    
+      case 'Incomplete':
+        return !todo.completed
+    
+      default:
+        return true
+    }
+  }
+
+  const filterTodoByColor = (todo)=>{
+    if(colors.length > 0){
+      return colors.includes(todo.color)
+    }
+    return true
+  }
+
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
 
 
       {
-        todos.map((todo, index) => <Todo todo={todo} key={index} />)
+        todos
+        .filter(filterTodoByStatus)
+        .filter(filterTodoByColor)
+        .map((todo, index) => <Todo todo={todo} key={index} />)
       }
 
 
